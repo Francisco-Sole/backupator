4d8115f3b1fcb751981e860343d0d745f86a63d7
Francisco Sole <srsole30687@gmail.com>
2025-10-07 10:55:28 +0200

    correcion de host

--- config.php
+++ config.php
@@ -1,11 +1,6 @@
 <?php
-// $host_C = '127.0.0.1';
-// $nombreDeBaseDeDatos_C = 'modulocliente';
-// $usuario_C = 'root';
-// $pasword_C = '';
 
-$host_C = '10.2.232.18';
-$nombreDeBaseDeDatos_C = 'modulocliente';
-$usuario_C = 'franc';
-$pasword_C = 'capsulecorp';
-?>
+$host_C = '127.0.0.1';
+$nombreDeBaseDeDatos_C = 'calendario';
+$usuario_C = 'root';
+$pasword_C = '';
